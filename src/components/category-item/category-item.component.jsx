8c280e668b589ed3ca921c73374e8107024c582a
gerrieod <gerrieod@gmail.com>
2023-12-17 09:32:23 +0200

    updating page with cartegories and new cateogry components

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,20 @@
+import "./category-item.style.scss"
+
+const CategoryItem = ({category}) => {
+    const {title, imageUrl} = category;
+    return(
+        <div className="category-container">
+          {/* background images of shopping cards */}
+          <div className="background-image" style={
+            { backgroundImage: `url(${imageUrl})`, }
+          } />
+            {/* Body of category card */}
+            <div className="category-body-container">
+              <h1>{title}</h1>
+              <p>Shop Now</p>
+            </div>
+          </div>
+    )
+}
+
+export default CategoryItem
